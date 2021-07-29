2c5348a6c67e4ece0a8d672ba43d1c359bc17be2
chris <cseiter@gmail.com>
2021-07-28 20:44:26 -0400

    changes to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,17 @@
-ENV["RACK_ENV"] ||= "development"
+# config/environment.rb
+require 'rake'
+require 'active_record'
+require 'yaml/store'
+require 'ostruct'
+require 'date'
 
 require 'bundler/setup'
-Bundler.require(:default, ENV["RACK_ENV"])
+Bundler.require
 
-require_relative "../app/models/artist"
+# put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+
+require_relative "../artist.rb" 
