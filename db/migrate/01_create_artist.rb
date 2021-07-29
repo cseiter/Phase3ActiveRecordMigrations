739e2acf99dcd40f90fc938624580c675628d012
chris <cseiter@gmail.com>
2021-07-28 20:47:38 -0400

    add create_table to ruby file

--- db/migrate/01_create_artist.rb
+++ db/migrate/01_create_artist.rb
@@ -1,10 +1,18 @@
 class CreateArtists <ActiveRecord::Migration[5.2]
-def up
-end
+    def up
+    end
 
-def down
-end
+    def down
+    end
 
-def change
-end
+    def change
+        create_table :artists {|t|
+            t.string :name
+            t.string :genre
+            t.integer :age
+            t.string :hometowm
+        }
+    end
+
+
 end
